61bdaefc176d0b5cda262dd62c6165af5d2f782d
arnaud.schaal <arnaud.schaal@gaming1.com>
2018-01-11 17:22:18 +0100

    Change Fragment files location.

--- app/src/main/java/com/example/arnaudschaal/ournation/Fragments/MainFragment.java
+++ app/src/main/java/com/example/arnaudschaal/ournation/Fragments/MainFragment.java
@@ -1,4 +1,4 @@
-package com.example.arnaudschaal.ournation;
+package com.example.arnaudschaal.ournation.Fragments;
 
 import android.content.Context;
 import android.net.Uri;
@@ -7,6 +7,8 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.arnaudschaal.ournation.R;
 
 public class MainFragment extends Fragment {
 
